f0c999b57c824457d4cfff02c3829ba8fcf27871
Ashwin-make <ashwinikarigar100@gmail.com>
2024-11-26 22:05:19 +0530

    Create Natural number from N to 1.c

---
+++ Natural number from N to 1.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+void main()
+{
+    int n,i=n;
+    printf("Enter the number of rows:");
+    scanf("%d",&n);
+    while(i>=1)
+      {
+            printf("*");
+         i--;
+      }
+}

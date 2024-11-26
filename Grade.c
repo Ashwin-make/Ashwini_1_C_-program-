4e062b542725b54db4d5dad56c848476656595a0
Ashwin-make <ashwinikarigar100@gmail.com>
2024-11-26 14:14:01 +0530

    Create Grade.c

---
+++ Grade.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+int marks;
+printf("Enter the marks");
+scanf("%d",&marks);
+if (marks>=90);{
+printf("Grade:A\n");
+}else if (marks>=75);{
+printf("Grade;B\n");
+}else if(mark>=50);
+printf(Grade;C\n);
+}else{
+printf(Grade:F\n);
+}
+return0;

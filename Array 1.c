f57a206f3064626fb8b32b882f8bfb1b6578cfe1
Ashwin-make <ashwinikarigar100@gmail.com>
2024-11-29 09:03:48 +0530

    Create Array 1.c

---
+++ Array 1.c
@@ -0,0 +1,32 @@
+include <stdio.h>
+
+int main() {
+    int size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter %d elements of the array:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int largest = arr[0], secondLargest = -1;
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    if (secondLargest == -1) {
+        printf("No second largest element found.\n");
+    } else {
+        printf("Second largest element: %d\n", secondLargest);
+    }
+
+    return 0;
+}
